83b1a02de70b44a6e8e1834b31436987c903cb4e
Yonatan Ahituv <ahituvyonatan@gmail.com>
2024-12-13 17:38:09 -0800

    Updated Van der Pol data generation

--- Vanderpol.m
+++ Vanderpol.m
@@ -15,7 +15,8 @@ p =rand();   % determines the frequency of oscilations
 
 y=rand();  % initial condtion
 x=rand();  % initial condtion
-out(1)=y;  % initial condtion
+out_x(1)=x;  % initial condtion
+out_y(1)=y;  % initial condtion
 
 
 dt = 0.01;   
@@ -28,12 +29,12 @@ for i= T_start:T_end
    u(i) = rand();
    x=dt*((landa-y.^2)*(x-p.^2*y + 5*u(i))) + x;
    y = dt*x + y;
-
-   out(i)=y;   % keeping the output in differernt time steps
+   out_x(i)=y;   % keeping the output in differernt time steps
+   out_y(i)=y;   % keeping the output in differernt time steps
 end
 data_out.u = u';
-data_out.x = out';
-data_out.y = out';
+data_out.x = out_x';
+data_out.y = out_y';
 data_out.t = time';
 data.train = [data.train data_out];
 end
@@ -44,8 +45,8 @@ save('datafiles/vanderpol.mat','-struct','data');
 figure;
 hold on
 
-plot (time, out)  % time domain
+plot (time, out_y)  % time domain
 xlabel('time');ylabel ('y')
 figure
-plot(out(1:end-1),diff(out))  % phase space
+plot(out_y(1:end-1),diff(out_y))  % phase space
 xlabel('y');ylabel ('x')
